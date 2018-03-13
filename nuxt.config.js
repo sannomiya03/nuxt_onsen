2623f94a6837a17288eeeb9dd569ef5a06c864df
Yasunori Sannomiya <y.sannomiya03@gmail.com>
2018-03-13 09:18:31 +0900

    ha

---
+++ nuxt.config.js
@@ -0,0 +1,48 @@
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'onsen',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/onsen/onsenui.css' },
+      { rel: 'stylesheet', href: '/onsen/onsen-css-components.css' }
+    ],
+    script: [
+      // { src: "/onsen/onsenui.js" },
+      // { src: "/onsen/vue-onsenui.js" }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    vendor: [
+      'vue-onsenui'
+    ],
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+     <v-ons-page>
+        <v-ons-toolbar>
+          <div class="left"><ons-toolbar-button @click="carouselIndex > 0 && carouselIndex--">i--</ons-toolbar-button></div>
+          <div class="center">Index: {{carouselIndex}}</div>
+          <div class="right"><ons-toolbar-button @click="carouselIndex < 2 && carouselIndex++">i++</ons-toolbar-button></div>
+        </v-ons-toolbar>
+
+        <v-ons-carousel fullscreen swipeable auto-scroll overscrollable
+          :index.sync="carouselIndex"
+        >
+          <v-ons-carousel-item v-for="(value, key) in items" :style="{backgroundColor: value}" :key="key">
+            <div style="text-align: center; font-size: 30px; margin-top: 20px; color: #fff;">{{key}}</div>
+          </v-ons-carousel-item>
+        </v-ons-carousel>
+
+        <div :style="dots">
+          <span :index="dotIndex - 1" v-for="dotIndex in Object.keys(items).length" :key="dotIndex" style="cursor: pointer" @click="carouselIndex = dotIndex - 1">
+            {{ carouselIndex === dotIndex - 1 ? '\u25CF' : '\u25CB' }}
+          </span>
+        </div>
+      </v-ons-page>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      carouselIndex: 0,
+      items: {
+        BLUE: '#085078',
+        DARK: '#373B44',
+        ORANGE: '#D38312'
+      },
+      dots: {
+        textAlign: 'center',
+        fontSize: '30px',
+        color: '#fff',
+        position: 'absolute',
+        bottom: '40px',
+        left: 0,
+        right: 0
+      }
+    };
+  }
+};
+</script>
